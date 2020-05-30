d9198410837902ec45924268ee82edb715d8a911
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-05-30 11:33:06 +0800

    Button part almost finished (Must think SQL twice)

--- sqlquery.php
+++ sqlquery.php
@@ -53,13 +53,71 @@ if(!empty($_POST['inputSQL'])){
 	}	  
 }else{ //BUTTON 結果
 
-	if(!empty($_POST['text1']) || !empty($_POST['text2']) || !empty($_POST['text3'])){	//再想想要怎麼判斷 0530
-		echo $_POST['text1']."<br>";
-		echo $_POST['text2']."<br>";
-		echo $_POST['text3']."<br>";	
-		echo $_POST['text4']."<br>";
-		echo $_POST['text5']."<br>";
+	if($_POST['tell_type'] == "HIDE"){
+		exit(0); // do nothing
+	}else if($_POST['tell_type'] == "SELECT"){
+		$query = "SELECT " . $_POST['text1'] ." FROM ". $_POST['text2'] . " WHERE " . $_POST['text3'];		
+	}else if($_POST['tell_type'] == "DELETE"){
+		$query = "DELETE FROM customer WHERE ". $_POST['text4'];
+	}else if($_POST['tell_type'] == "INSERT"){
+		$query = "INSERT INTO customer ". $_POST['text5']." VALUES ". $_POST['text6'];	
+	}else if($_POST['tell_type'] == "UPDATE"){
+		$query = "UPDATE customer SET ". $_POST['text7']." WHERE ". $_POST['text8'];
+	}else if($_POST['tell_type'] == "IN"){
+		$query = "SELECT * FROM customer WHERE customer_id > 3 IN (". $_POST['text9']." )";
+	}else if($_POST['tell_type'] == "NOT_IN"){
+		$query = "SELECT * FROM customer WHERE customer_id > 4 NOT IN (". $_POST['text10']." )";
+	}else if($_POST['tell_type'] == "EXISTS"){
+		$query = "SELECT " . $_POST['text11'] . " FROM customer WHERE EXISTS (" . $_POST['text12']." )";
+	}else if($_POST['tell_type'] == "NOT_EXISTS"){
+		$query = "SELECT " . $_POST['text13']." FROM customer WHERE NOT EXISTS (". $_POST['text14']." )";
+	}else if($_POST['tell_type'] == "COUNT"){
+		$query = "SELECT COUNT(". $_POST['text15']. ") FROM merchandise";
+	}else if($_POST['tell_type'] == "SUM"){
+		$query = "SELECT SUM(" . $_POST['text16'].") FROM merchandise";
+	}else if($_POST['tell_type'] == "MAX"){
+		$query = "SELECT MAX(". $_POST['text17'].") FROM merchandise";
+	}else if($_POST['tell_type'] == "MIN"){
+		$query = "SELECT MIN(". $_POST['text18'].  ") FROM merchandise";
+	}else if($_POST['tell_type'] == "AVG"){
+		$query = "SELECT AVG(". $_POST['text19'].  ") FROM merchandise";
+	}else{ //HAVING
+		$query = "SELECT ". $_POST['text20']." FROM ". $_POST['text21']." GROUP BY ". $_POST['text22']." HAVING ". $_POST['text23'];
 	}
+
+
+	echo "<span class=\"span_show\"> 您輸入的指令 " . $query . "</span>";
+	
+	try{
+		$result = $conn->query($query);	
+		echo "<span class=\"span_show\">" . "<br> Button處理結果為 ". $result->num_rows. " 筆資料" . "</span>";
+		echo "<table border='1'> 
+			  <tr>";	
+		$j = 0;	  
+		while($mysql_query_fields = mysqli_fetch_field($result)){ // 取得field name
+			$mysql_fields[$j] = $mysql_query_fields->name;
+			echo "<th>". $mysql_fields[$j]. "</th>";		
+			$j = $j + 1;
+		}
+		echo "</tr>";
+		
+		if ($result->num_rows > 0){
+			while($row = $result->fetch_row()){
+				echo "<tr>";
+				$i = 0;
+				for(;$i < mysqli_num_fields($result); $i = $i + 1){
+					echo "<td>" . $row[$i] . "</td>";
+				}
+				echo "</tr>";
+			}	
+		} else {
+			echo "0 results";
+		}
+	}
+	catch(Exception $e){
+	  echo 'something wrong!';
+
+	}	
 	
 }
 
